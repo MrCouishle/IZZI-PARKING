64bbe8cd43c37445b6726cd607808ceb56f80c62
Cristian David Hernandez Suarez <75052459+Parzeval8@users.noreply.github.com>
2022-11-16 15:22:45 -0500

    ...

---
+++ IP-F/src/pdf/reportentrys.js
@@ -0,0 +1,117 @@
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import pdfMake from "pdfmake/build/pdfmake";
+import { formarNumber_ } from "../global";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import moment from "moment";
+
+var estructure_image = (image) => {
+  let format = { width: "15%", margin: [-5, 5, 0, 0] };
+  if (!image) format.stack = [{ text: "" }];
+  else {
+    format.fit = [50, 50];
+    format.image = image;
+  }
+
+  return { ...format };
+};
+
+var tableBody = (listado = []) => {
+  let items_pdf = [];
+  console.log(listado);
+
+  listado.forEach((item) => items_pdf.push(format_item(item)));
+
+  console.log("------------", items_pdf);
+
+  return items_pdf;
+};
+var format_item = (item) => {
+  return [
+    { text: item.date_init, fontSize: 7, alignment: "center" },
+    { text: item.time_init, fontSize: 7, alignment: "center" },
+    { text: item.type_vehicle, fontSize: 7, alignment: "center" },
+    { text: item.placa, fontSize: 7, bold: true, alignment: "left" },
+    { text: item.puesto, fontSize: 7, bold: true, alignment: "center" },
+    { text: item.date_end, fontSize: 7, alignment: "center" },
+    { text: item.time_end, fontSize: 7, alignment: "center" },
+  ];
+};
+export default function ({ items, header, fecha_ini, fecha_fin }) {
+  console.log(header);
+  return new Promise((resolve) => {
+    let headers = ["Fecha inicio", "Hora ingreso", "Tipo vehiculo", "Placa", "Puesto", "Fecha salida", "Hora salida"].map((el) => {
+      return { text: el, fontSize: 9, bold: true, alignment: "center" };
+    });
+    var dd = {
+      info: {
+        title: "Reportes IZZI-PARKING",
+        author: "Cristian",
+      },
+      // userPassword: "IZZIPARKING",
+      watermark: { text: "IZZI-PARKING", color: "gray", opacity: 0.2 },
+      pageMargins: [20, 95, 20, 60],
+      header: function (currentPage, pageCount) {
+        return [
+          {
+            margin: [40, 30, 40, 0],
+            columns: [
+              estructure_image(header.logo),
+              {
+                alignment: "center",
+                stack: [
+                  { text: header.name, bold: true, fontSize: 16 },
+                  { text: "REPORTES DE INGRESOS IZZIPARKING", bold: true, fontSize: 12 },
+                  {
+                    text: header.author + " " + header.document,
+                    bold: true,
+                    fontSize: 9,
+                  },
+                  {
+                    text: `Fecha de impresión: ${moment().format("DD/MM/YYYY")} Hora de impresión: ${moment().format("LTS")}`,
+                    bold: true,
+                    fontSize: 9,
+                  },
+                ],
+              },
+              {
+                width: "20%",
+                alignment: "right",
+                stack: [{ text: `Página ${currentPage}/${pageCount}`, bold: true, fontSize: 10 }],
+              },
+            ],
+          },
+          {
+            canvas: [
+              {
+                type: "rect",
+                x: 20,
+                y: -55,
+                w: 555,
+                h: 65,
+              },
+            ],
+          },
+        ];
+      },
+      content: [
+        {
+          style: "tableExample",
+          marginTop: 25,
+          fillColor: "#EEEEEF",
+          table: {
+            widths: ["15%", "15%", "15%", "15%", "15%", "15%", "10%"],
+            body: [headers, ...tableBody(items)],
+          },
+          width: "100%",
+        },
+      ],
+    };
+    try {
+      pdfMake.createPdf(dd).download("Reportes IZZI-PARKING");
+      pdfMake.createPdf(dd).open();
+    } catch (error) {
+      console.error(error);
+    }
+    resolve(true);
+  });
+}
--- IP-F/src/views/user/Helps.vue
+++ IP-F/src/views/user/Helps.vue
@@ -2,72 +2,96 @@
   <v-container fluid>
     <v-row justify="center" class="pt-4 ml-16 mr-2">
       <v-col cols="12" lg="12" sm="12" md="12">
-          <v-card elevation="4">
-            <v-card-text class="text-title primary--text mt-0">
-              <h2 class="text-start">
-                <v-icon color="primary" class="mb-1">mdi-video</v-icon>
-                Ayuda > Video guías
-              </h2>
-            </v-card-text>
-          </v-card>
+        <v-card elevation="4">
+          <v-card-text class="text-title primary--text mt-0">
+            <h2 class="text-start">
+              <v-icon color="primary" class="mb-1">mdi-video</v-icon>
+              Ayuda > Video guías
+            </h2>
+          </v-card-text>
+        </v-card>
       </v-col>
-      <v-col cols="6" lg="6" sm="6" md="6" v-for=" (item , i) in data" :key="i">
+      <v-col cols="6" lg="6" sm="6" md="6" v-for="(item, i) in data" :key="i">
         <v-card elevation="12">
           <v-card-text class="primary--text">
             <v-list>
               <v-list-item>
                 <v-list-item-content>
                   <v-list-item-title class="text-title">
-                    <v-chip  color="primary">
-                      {{ item.title}} 
+                    <v-chip color="primary">
+                      {{ item.title }}
                     </v-chip>
                   </v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
               <v-divider></v-divider>
               <v-list-item class="ml-0">
-                <v-list-item-title class="link"> <iframe width="500" height="250" :src="item.video" title="Cómo registrarse e iniciar sesión" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> </v-list-item-title>
+                <v-list-item-title class="link">
+                  <iframe
+                    width="500"
+                    height="250"
+                    :src="item.video"
+                    title="Cómo registrarse e iniciar sesión"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
+                </v-list-item-title>
               </v-list-item>
-             
+
               <v-list-item class="ml-0">
-                <v-list-item-title class="text-title">  {{item.descrip}} </v-list-item-title>
+                <v-list-item-title class="text-title"> {{ item.descrip }} </v-list-item-title>
               </v-list-item>
             </v-list>
           </v-card-text>
         </v-card>
       </v-col>
+      <v-btn color="primary" fab dark  fixed bottom right @click="chat.state = true">
+        <v-icon>mdi-face-agent</v-icon>
+      </v-btn>
     </v-row>
+    <Chat :chat="chat" v-if="chat.state" />
   </v-container>
 </template>
 <script>
+import Chat from "../../components/help/Chat.vue";
 export default {
   name: "help",
+  components: {
+    Chat,
+  },
   data() {
     return {
+      chat: {
+        state: false,
+      },
       data: [
         {
           title: "Guía #1: Como registrarse e Iniciar sesión en IZZI PARKING",
           video: "https://www.youtube.com/embed/_QAN0ozZlE4",
-          descrip: "Descripción: Como primer paso se hará click en el botón de crear cuenta donde se desplegará una ventana donde se diligenciaran los datos que estén para poder así crear la cuenta y así seguir a iniciar sesión con el correo y con la contraseña asignada  ",
+          descrip:
+            "Descripción: Como primer paso se hará click en el botón de crear cuenta donde se desplegará una ventana donde se diligenciaran los datos que estén para poder así crear la cuenta y así seguir a iniciar sesión con el correo y con la contraseña asignada  ",
         },
         {
           title: "Guía #2: Como realizar reservas en IZZI PARKING",
           video: "https://www.youtube.com/embed/EWusa3CS9aM",
-          descrip: "Descripción: Ya dentro del aplicativo nos dirigiremos a el menú lateral y le daremos click a las reservas donde tendremos que llenar unos datos. Adicional como administrador podremos visualizar las reservas en el calendario.",
+          descrip:
+            "Descripción: Ya dentro del aplicativo nos dirigiremos a el menú lateral y le daremos click a las reservas donde tendremos que llenar unos datos. Adicional como administrador podremos visualizar las reservas en el calendario.",
         },
         {
           title: "Guía #3: Como configurar tu perfil en IZZI PARKING",
           video: "https://www.youtube.com/embed/Jum3MVMfvoM",
-          descrip: "Descripción: Ya dentro del aplicativo nos dirigiremos a el menú lateral y le daremos click al botón de perfil y en ese apartado podremos configurar nuestro perfil y realizar cambios ",
+          descrip:
+            "Descripción: Ya dentro del aplicativo nos dirigiremos a el menú lateral y le daremos click al botón de perfil y en ese apartado podremos configurar nuestro perfil y realizar cambios ",
         },
         {
           title: "Guía #4: Como visualizar puestos disponibles en IZZI PARKING",
           video: "https://www.youtube.com/embed/aLAGiwlJ30s",
-          descrip: "Descripción: Ya dentro del aplicativo nos dirigiremos a el menú lateral y le daremos click al botón de gestión de puestos y a continuación al de puestos disponibles donde nos mostrará los apartados de los parqueaderos de carro y moto, donde se señalara que los verdes son disponibles, rojos ocupados y amarillos reservados.",
+          descrip:
+            "Descripción: Ya dentro del aplicativo nos dirigiremos a el menú lateral y le daremos click al botón de gestión de puestos y a continuación al de puestos disponibles donde nos mostrará los apartados de los parqueaderos de carro y moto, donde se señalara que los verdes son disponibles, rojos ocupados y amarillos reservados.",
         },
       ],
     };
   },
 };
 </script>
-  
